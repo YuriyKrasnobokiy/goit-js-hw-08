116cbb354065008578290d43a108c065c9341164
Yuriy Krasnobokiy <krasnobokiy.yuriy@gmail.com>
2023-08-12 22:34:46 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const key = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(handlerOnPlay, 1000));
+function handlerOnPlay(data) {
+  const time = data.seconds;
+  localStorage.setItem(key, time);
+}
+
+player.setCurrentTime(localStorage.getItem(key));
